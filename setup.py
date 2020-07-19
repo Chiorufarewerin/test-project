16da8220c9e95ccb5cf1ed2cdaa8a5342488d89e
Artur Beltsov <artur1998g@gmail.com>
2020-07-19 18:00:42 +0500

    0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize
 
 
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 EXTENSIONS = cythonize(
     Extension(
         'test_project.test',
